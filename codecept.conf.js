7868a862bde328b719d0db51d3852586a73a8a11
Nadine Heidrich <nheidric@thoughtworks.com>
2017-03-27 14:27:11 +0200

    Add pngs to ingnore list and put firefox as default browser in conf | nh

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,27 +1,8 @@
 exports.config = {
     helpers: {
         WebDriverIO: {
-            host: 'hub.crossbrowsertesting.com',
-            port: 80,
-            path: '/wd/hub',
             url: 'http://www.thoughtworks.de',
-            browser: 'internet explorer',
-            coloredLogs: true,
-            desiredCapabilities: {
-                name : 'TW Demo',
-                build : '0.1',
-
-                browserName : 'internet explorer',
-                browser_api_name : 'IE10',
-                os_api_name : 'Win7x64-C2',
-                screen_resolution : '1280x1024',
-
-                record_video : 'true',
-                record_network : 'true',
-
-                username : 'username',
-                password : 'authkey'
-            }
+            browser: 'firefox'
         }
     },
     tests: './*_test.js',
